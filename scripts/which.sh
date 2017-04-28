4f5f8ac3479c6b3873a68810604f22d75b3a324f
h-dh <hollweg@dkrz.de>
2017-04-28 16:33:25 +0200

    never update conda packages; user may opt for other updates

--- scripts/which.sh
+++ scripts/which.sh
@@ -1,56 +1,74 @@
 
 # which for path and alias, even if non-interactive
 
-which()
+this_which()
 {
-  local isAdd cm cmd f i j last
-  declare -a cmd f
+  local isAdd cm cmd f found i j k last lines
+  declare -a cmd f found lines
 
+  # try which
+  found=( $( which -a $1 2> /dev/null ) )
+
+  # files which could have aliases
   f[0]=${HOME}/.bashrc
 
-  if ! cmd=( $(/usr/bin/which $1 2> /dev/null) ) ; then
+  local saveIFS
+  saveIFS="${IFS}"
+
     for(( i=${#f[*]}-1 ; i > -1 ; --i )) ; do
       # scan for alias. Note such is not effective here
-      cmd=( $(grep alias ${f[i]}) )
-      unset f[${i}]
+      IFS=$'\r\n'
+      lines=( $(grep alias ${f[i]}) )
+      IFS="${saveIFS}"
+
+      unset f[i]
       f=( ${f[*]} )
       isAdd=t
 
-      for(( j=1 ; j < ${#cmd[*]} ; ++j )) ; do
-        cm=${cmd[j]}
-        test ${cm:0:1} = '~' && cm=${HOME}${cm:1}
+      for(( k=0 ; k < ${#lines[*]} ; ++k )) ; do
+        cmd=( ${lines[k]} )
+        test ${cmd[0]:0:1} = '#' && continue
 
-        if [ ${isAdd} = t -a -f ${cm} ] ; then
-          f[${#f[*]}]=${cm}
-          isAdd=f
-        else
-          test ${cmd[j]} = 'alias'  && continue
-          test ${cmd[$((j-1))]} != 'alias'  && continue
+        # is a file executed by source or . ?
+        for(( j=0 ; j < ${#cmd[*]} ; ++j )) ; do
+           if [ "${cmd[j]}" = '.' -o "${cmd[j]}" = 'source' ] ; then
+              j=$((j+1))
+              cm=${cmd[j]//\~/${HOME}}
+              if [ -f ${cm} ] ; then
+                f[${#f[*]}]=${cm}
+                isAdd=f
+              fi
+           fi
+        done
 
-          cm=${cmd[j]#*=}
-          test ${cm:0:1} = \' -o ${cm:0:1} = '"' && cm=${cm:1}
+        for(( j=0 ; j < ${#cmd[*]} ; ++j )) ; do
+           # search alias
+           if [ ${cmd[j]//$1/} != ${1} ] ; then
+              # found an alias containing the command in question
+              cm=${cmd[j]#*=}
+              test ${cm:0:1} = \' -o ${cm:0:1} = '"' && cm=${cm:1}
 
-          last=$((${#cm} - 1 ))
-          test ${cm:last} = \' -o ${cm:last} = '"' && cm=${cm:0:last}
+              last=$((${#cm} - 1 ))
+              test ${cm:last} = \' -o ${cm:last} = '"' && cm=${cm:0:last}
 
-          if [ ${cm##*/} = $1 ] ; then
-            cmd=${cm}
-            break 2
-          fi
-        fi
+              found[${#found[*]}]=${cm}
+              break
+           fi
+        done
       done
 
-      if [ ${#f[*]} -eq 0 ] ; then
-        echo "no such command or alias $1"
-        exit 1
-      fi
+#      if [ ${#f[*]} -eq 0 ] ; then
+#        echo "no such command or alias $1"
+#        return 1
+#      fi
 
       i=${#f[*]}
     done
-  fi
 
-  echo "${cmd}"
-  return
+  echo ${found[*]}
+  return 0
 }
 
-which $1
+this_which $1
+
+return $?
